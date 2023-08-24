b193726d4b405414aa52d7af2224fd8ad075811a
Kshitiz Bhargava <kshitizbhargava2626@gmail.com>
2023-08-24 12:06:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  void playSound(int note) {
+    final player = AudioPlayer();
+    player.setSource(AssetSource('note$note.wav'));
+    player.resume();
+  }
+
+  Expanded buildKey(Color color, int soundNumber) {
+    return Expanded(
+      child: MaterialButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        color: color,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
